dd054a0026be71d546971de95db597dad8e11c38
Eric Henderson <henderea@rose-hulman.edu>
2012-11-07 12:53:00 -0500

    updated shape.h

--- src/shape.h
+++ src/shape.h
@@ -10,6 +10,7 @@
 #define shape_h
 
 #include "transform.h"
+#include "ShapeVertices.h"
 
 typedef enum {SHAPE_CUBE, SHAPE_SPHERE, SHAPE_TETRAHEDRON, SHAPE_PLANE, SHAPE_OCTAHEDRON/*, SHAPE_ICOSAHEDRON*/, SHAPE_CYLINDER} shape_type;
 
@@ -30,6 +31,11 @@ struct Shape
 struct ShapeCube : Shape
 {
     ShapeCube(unsigned int shapeId, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : Shape(SHAPE_CUBE, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawCube();
+    }
 };
 
 struct ShapeSphere : Shape
@@ -38,6 +44,11 @@ struct ShapeSphere : Shape
     int vseg;
     
     ShapeSphere(unsigned int shapeId, int hseg, int vseg, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : hseg(hseg), vseg(vseg), Shape(SHAPE_CUBE, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawSphere(hseg, vseg);
+    }
 };
 
 struct ShapeCylinder : Shape
@@ -45,21 +56,41 @@ struct ShapeCylinder : Shape
     int seg;
     
     ShapeCylinder(unsigned int shapeId, int seg, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : seg(seg), Shape(SHAPE_CYLINDER, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawCylinder(seg);
+    }
 };
 
 struct ShapeTetrahedron : Shape
 {
     ShapeTetrahedron(unsigned int shapeId, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : Shape(SHAPE_TETRAHEDRON, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawTetrahedron();
+    }
 };
 
 struct ShapePlane : Shape
 {
     ShapePlane(unsigned int shapeId, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : Shape(SHAPE_PLANE, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawPlane();
+    }
 };
 
 struct ShapeOctahedron : Shape
 {
     ShapeOctahedron(unsigned int shapeId, TransformTranslate trans, TransformRotate rot, TransformScale scale/*, TransformShear shear*/) : Shape(SHAPE_OCTAHEDRON, shapeId, trans, rot, scale/*, shear*/) {}
+    
+    void drawShape()
+    {
+        drawOctahedron();
+    }
 };
 
 #endif
